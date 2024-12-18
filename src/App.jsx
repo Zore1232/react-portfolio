f054819d250df0ae4edf25494c18df85b94b88a4
Zore1232 <156526701+Zore1232@users.noreply.github.com>
2024-12-18 13:36:53 +0630

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './components/NavBar'
+import NavBar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
 import Technology from './components/Technology'
